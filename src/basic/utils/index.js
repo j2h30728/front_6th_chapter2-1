17327a373892f1ef725543f006e27470e7f516de
jihyeon <rachel2148072@gmail.com>
2025-08-01 00:26:37 +0900

    fix: 잘못된 export 구문 수정

--- src/basic/utils/index.js
+++ src/basic/utils/index.js
@@ -1,4 +1,4 @@
-export { createObserver } from './createObserver.js';
-export { createStore } from './createStore.js';
+export { default as createObserver } from './createObserver.js';
+export { default as createStore } from './createStore.js';
 export { formatNumber, formatPrice, safeParseInt, when, whenValue } from './dataUtils.js';
 export { getElement, querySelector, setInnerHTML, setStyle, setTextContent } from './domUtils.js';
