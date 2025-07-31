1bf98bb197cc426cf0f13e1e5039e2965721a552
jihyeon <rachel2148072@gmail.com>
2025-08-01 04:39:52 +0900

    build: 빌드 및 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
+/* eslint-disable no-undef */
 import { fileURLToPath, URL } from 'node:url';
 
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 import { defineConfig } from 'vite';
 
+const base = process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-1/' : '';
+
+const entryFileName = 'index.advanced.html';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react()],
+  base,
+  plugins: [
+    react(),
+    {
+      name: 'rename-html-output',
+      closeBundle() {
+        const from = path.resolve(__dirname, `dist/${entryFileName}`);
+        const to = path.resolve(__dirname, 'dist/index.html');
+        if (fs.existsSync(from)) fs.renameSync(from, to);
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src/advanced', import.meta.url)),
@@ -14,5 +33,13 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.js'],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, entryFileName),
+        404: path.resolve(__dirname, '404.html'),
+      },
+    },
   },
 });
